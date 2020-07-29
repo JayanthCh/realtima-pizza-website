c1d8e73d4d3b2f95945efb75e13d046f1d871187
Jayanth Chinimilli <jayanthchinimilli33@gmail.com>
2020-07-29 16:54:25 +0530

    initial

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const ejs = require("ejs");
+const path =require("path")
+const app = express();
+
+
+app.get("/",function(req,res){
+  res.render("home");
+})
+
+
+
+
+app.use(expressLayout);
+app.set("views",path.join(__dirname,'/resourses/views'));
+app.set("view engine","ejs");
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT||3000;
+app.listen(PORT,function(){
+  console.log(`server is running on port ${PORT}`);
+})
